c939d6b8cff936705de3da7abc4a6500bd76b349
Brayan Bernal <brayanbernal0710@gmail.com>
2019-06-15 12:32:53 -0400

    agregando ultimos cambios pagina finalizada

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const faker = require('faker');
-const mysql = require('mysql');
 const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -11,4 +17,35 @@ const connection = mysql.createConnection({
 
 
 
-connection.end();
+app.get('/',function(req,res){
+    let query = 'SELECT COUNT(*) AS total FROM users';
+    connection.query(query, (err,result)=>{
+        if (err) throw err;
+        let count = result[0].total;
+        res.render('home', {count: count} );
+    });
+});
+
+app.post('/register',function(req,res){
+    const person = { email: req.body.email };
+        
+    connection.query('INSERT INTO users SET ?', person, (err,result)=>{
+        if (err) throw err;
+        console.log(result);
+    });
+    res.redirect('/');
+});
+
+app.get('/register',function(req,res){
+    res.redirect('/');
+});
+
+app.get('*',function(req,res){
+    res.send(`I'm Sorry I don't got that URL.`)
+});
+
+app.listen(port,()=>{
+    console.log(`Running at Port: ${port}`);
+});
+
+// connection.end();
